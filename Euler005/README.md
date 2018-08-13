c4b7870fe2feb7a7cc86d12d805cf6d6fca96c95
ppedra <asb@asd.com>
2018-08-13 17:06:06 -0300

    problem 005 done

--- Euler005/README.md
+++ Euler005/README.md
@@ -2,3 +2,6 @@
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+## comemnts on the solution:
+it took a long time! 240.05s (4min) to be precise! i should revisit this problem later
--- Euler005/euler005.py
+++ Euler005/euler005.py
@@ -1,10 +1,29 @@
-max_num = 10
+"""
+Euler 005
+"""
+import timeit
 
-possible_number = 1
-while True:
-    for i in range(1,max_num+1):
-        if (possible_number%i==0):
-            print(possible_number)
+def is_dividable_by_range(lim):
+    print("check which number is evenly divisible by",list(range(1,lim+1)))
+
+    #dumb way
+    num = 1
+    while True:
+        is_dividable = True
+
+        for i in range(1,lim+1):
+            if num%i!=0:
+                is_dividable=False
+                break
+
+        if is_dividable:
+            print(num,"is evenly divisible by",list(range(1,lim+1)))
+            print("--")
+            break
         
-    possible_number+=1
-    
+        num+=1
+
+if __name__ == "__main__":
+    print(timeit.timeit("is_dividable_by_range(20)",globals=globals(),number=1))
+
+        
